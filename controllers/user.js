b2d8f713a3d15ad9588acdb8bcd34af61ee135d2
Wasif-Ali47 <wasifaliwa7367881@gmail.com>
2026-02-10 11:36:49 +0500

    auth update

--- controllers/user.js
+++ controllers/user.js
@@ -15,32 +15,20 @@ async function handleGetAllUsers(req, res) {
 
 async function handlePostNewUser(req, res) {
     const body = req.body;
-    if (!body) {
-        return res.status(400).json({ message: ALL_FILEDS_REQUIRED })
-    } else if (!body.name) {
-        return res.status(400).json({ message: NAME_REQUIRED })
-    } else if (!body.email) {
-        return res.status(400).json({ message: EMAIL_REQUIRED })
-    } else if (!body.profession) {
-        return res.status(400).json({ message: PROFESSION_REQUIRED })
-    } else if (!body.password) {
-        return res.status(400).json({ message: PASSWORD_REQUIRED })
-    } else {
-        try {
-            const result = await User.create({
-                name: body.name,
-                email: body.email,
-                profession: body.profession,
-                password: body.password
-            });
-            console.log("result:", result)
-            res.status(201).json({ success: USER_CREATED });
-        } catch (err) {
-            console.error("DB create error:", err);
-            res.status(500).json({ error: NETWORK_ERROR });
-        }
+    if (!body) return res.status(400).json({ message: ALL_FILEDS_REQUIRED });
+    if (!body.name) return res.status(400).json({ message: NAME_REQUIRED });
+    if (!body.email) return res.status(400).json({ message: EMAIL_REQUIRED });
+    if (!body.profession) return res.status(400).json({ message: PROFESSION_REQUIRED });
+    if (!body.password) return res.status(400).json({ message: PASSWORD_REQUIRED });
 
-    }
+    const result = await User.create({
+        name: body.name,
+        email: body.email,
+        profession: body.profession,
+        password: body.password
+    });
+    console.log("result:", result);
+    res.status(201).json({ success: USER_CREATED });
 }
 
 
@@ -69,7 +57,6 @@ async function handleUpdateUserById(req, res) {
     if (!body.profession) return res.status(400).json({ message: PROFESSION_REQUIRED });
     if (!body.password) return res.status(400).json({ message: PASSWORD_REQUIRED });
 
-    try {
         const user = await User.findByIdAndUpdate(
             req.params.id,
             body,
@@ -78,10 +65,7 @@ async function handleUpdateUserById(req, res) {
 
         if (!user) return res.status(404).json({ error: USER_NOT_FOUND });
         res.json({ success: USER_UPDATED });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: NETWORK_ERROR });
-    }
+
 }
 
 
